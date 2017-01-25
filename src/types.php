29facdd071687b8985d10ddaf26853dec0296aaf
oneeyedjames <jascott@programmer.net>
2017-01-25 16:51:22 -0500

    Type function polyfills, for real this time

---
+++ src/types.php
@@ -0,0 +1,25 @@
+<?php
+
+if (!function_exists('boolval')) {
+	function boolval($var) {
+		return (bool)$var;
+	}
+}
+
+if (!function_exists('intval')) {
+	function intval($var) {
+		return (int)$var;
+	}
+}
+
+if (!function_exists('floatval')) {
+	function floatval($var) {
+		return (float)$var;
+	}
+}
+
+if (!function_exists('stringval')) {
+	function stringval($var) {
+		return (string)$var;
+	}
+}
