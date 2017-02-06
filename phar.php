f2223dd3ac2af39362cc816cc8dd453f4a52bf33
oneeyedjames <jascott@programmer.net>
2017-02-06 08:15:26 -0500

    Updated build script

--- phar.php
+++ phar.php
@@ -1,19 +1,16 @@
 <?php
-$srcRoot = './src';
-$buildRoot = './build';
-$buildFile = 'zombie.phar';
+
+$srcRoot = 'src';
+$buildRoot = 'build';
+$buildFile = basename(__DIR__);
 
 $flags = FilesystemIterator::CURRENT_AS_FILEINFO | FilesystemIterator::KEY_AS_FILENAME;
 
-$phar = new Phar("$buildRoot/$buildFile", $flags, $buildFile);
+$phar = new Phar("$buildRoot/$buildFile.phar", $flags, $buildFile);
 
 foreach (glob('src/*.php') as $srcFile) {
-	echo "$srcFile\n";
+	echo "Including $srcFile\n";
 	$phar->addFile($srcFile, basename($srcFile));
 }
 
-//$phar["index.php"] = file_get_contents($srcRoot . "/index.php");
-//$phar["common.php"] = file_get_contents($srcRoot . "/common.php");
-//$phar->setStub($phar->createDefaultStub("index.php"));
-
-//copy($srcRoot . "/config.ini", $buildRoot . "/config.ini");
+echo "...\nFinished $buildRoot/$buildFile.phar\n";
