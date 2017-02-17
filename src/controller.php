34b8f67ae538f4a1195192ba73ecafdb81b9dd1d
oneeyedjames <jascott@programmer.net>
2017-02-17 11:26:25 -0500

    Refined DB querying and results

--- src/controller.php
+++ src/controller.php
@@ -21,17 +21,21 @@ class controller_base {
 	}
 
     public function do_action($action) {
-		$action = str_replace('-', '_', $action);
-        $method = $action . '_action';
-
+		$method = str_replace('-', '_', $action) . '_action';
         if (method_exists($this, $method))
             return call_user_func(array($this, $method), $_GET, $_POST);
         else
             trigger_error("Undefined action $this->resource:$action", E_USER_WARNING);
     }
 
-	protected function query($sql, $params = array(), &$found = null) {
-		return $this->_database->query($sql, $params, $found);
+	public function pre_view($view, &$vars) {
+		$method = str_replace('-', '_', $view) . '_view';
+		if (method_exists($this, $method))
+			$vars = call_user_func(array($this, $method), $vars);
+	}
+
+	protected function query($sql, $params = array()) {
+		return $this->_database->query($sql, $params);
 	}
 
 	protected function execute($sql, $params = array()) {
